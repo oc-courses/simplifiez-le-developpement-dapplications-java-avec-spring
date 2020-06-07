6c244e3882958d8a827fb64a32c2c6ce7a809b4e
PierreSQS <nomo_messina@web.de>
2020-06-07 15:15:59 +0200

    Commit 04-OK: Annotations sur toute l'appli
    sur webapp et batch

--- ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
+++ ticket/ticket-batch/src/main/java/org/example/demo/ticket/batch/Main.java
@@ -1,9 +1,15 @@
 package org.example.demo.ticket.batch;
 
 
+import java.util.Arrays;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.example.demo.ticket.business.manager.factory.contract.ManagerFactory;
 import org.example.demo.ticket.model.exception.TechnicalException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 /**
@@ -24,21 +30,37 @@ public class Main {
      * @throws TechnicalException sur erreur technique
      */
     public static void main(String[] pArgs) throws TechnicalException {
-        try {
+        
+    	ConfigurableApplicationContext appCtx = 
+        		new AnnotationConfigApplicationContext("org.example.demo.ticket");
+        
+    	try {
             if (pArgs.length < 1) {
                 throw new TechnicalException("Veuillez préciser le traitement à effectuer !");
             }
 
             String vTraitementId = pArgs[0];
+            
             if ("ExportTicketStatus".equals(vTraitementId)) {
+            	
                 LOGGER.info("Execution du traitement : ExportTicketStatus");
-                // ...
+                ManagerFactory vMgrFct = appCtx.getBean("managerFactory", ManagerFactory.class);
+                
+                int beansCnt = appCtx.getBeanDefinitionCount();
+                LOGGER.info("Number of Beans: "+beansCnt);
+                
+                LOGGER.info("The created Beans... ");
+                Arrays.asList(appCtx.getBeanDefinitionNames())
+                	.forEach(name -> LOGGER.info(name));
+                
             } else {
                 throw new TechnicalException("Traitement inconnu : " + vTraitementId);
             }
         } catch (Throwable vThrowable) {
             LOGGER.error(vThrowable);
             System.exit(1);
-        }
+        } finally {
+			appCtx.close();
+		}
     }
 }
