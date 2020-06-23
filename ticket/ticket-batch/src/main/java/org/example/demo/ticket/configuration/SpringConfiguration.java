fda861015526e908bf41f335c2703dd86a6f23dc
PierreSQS <nomo_messina@web.de>
2020-06-23 22:10:36 +0200

    Commit 22annjava-OK: Annotation Java.
    Hurrah!!!! ticket-batch.Main fonctionne. Ajout de la dépendance
    BasicDataSource.Paramètre <context:property-placeholder/> bien défini

--- ticket/ticket-batch/src/main/java/org/example/demo/ticket/configuration/SpringConfiguration.java
+++ ticket/ticket-batch/src/main/java/org/example/demo/ticket/configuration/SpringConfiguration.java
@@ -3,6 +3,7 @@ package org.example.demo.ticket.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
+// Scan dans le code de ticket-batch.Main
 @Configuration
 @ImportResource(locations = "classpath:/org/example/demo/ticket/batch/batchContext.xml")
 public class SpringConfiguration {
