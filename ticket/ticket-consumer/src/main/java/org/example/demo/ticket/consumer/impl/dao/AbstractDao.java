ed5dc09b5ff06df82abe2f7eda85e7f66fe3c604
PierreSQS <nomo_messina@web.de>
2020-07-06 21:13:27 +0200

    Commit 25annjava-NOK: Annotation Java. Préparation pour refactoring DAO

--- ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/impl/dao/AbstractDao.java
+++ ticket/ticket-consumer/src/main/java/org/example/demo/ticket/consumer/impl/dao/AbstractDao.java
@@ -4,9 +4,17 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public abstract class AbstractDao {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private NamedParameterJdbcTemplate namedParamJdbcTemplate;
+	
 	@Autowired
 	@Qualifier("dataSourceTicket")
 	private DataSource dataSource;
@@ -17,6 +25,22 @@ public abstract class AbstractDao {
 
 	protected DataSource getDataSource() {
 		return dataSource;
+	}
+
+	protected JdbcTemplate getJdbcTemplate() {
+		return jdbcTemplate;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	protected NamedParameterJdbcTemplate getNamedParamJdbcTemplate() {
+		return namedParamJdbcTemplate;
+	}
+
+	public void setNamedParamJdbcTemplate(NamedParameterJdbcTemplate namedParamJdbcTemplate) {
+		this.namedParamJdbcTemplate = namedParamJdbcTemplate;
 	}
 
 }
