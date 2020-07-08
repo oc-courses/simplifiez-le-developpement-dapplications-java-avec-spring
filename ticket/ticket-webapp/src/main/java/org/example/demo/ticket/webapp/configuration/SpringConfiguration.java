0395a6898b66c6473911a221512233bf49fda87f
PierreSQS <nomo_messina@web.de>
2020-07-08 12:34:17 +0200

    Commit 27annjava-OK: Annotation Java. Adaptations pour ticket-webapp
    ticket-webapp se connecte aussi à la DB

--- ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/configuration/SpringConfiguration.java
+++ ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/configuration/SpringConfiguration.java
@@ -1,12 +1,29 @@
 package org.example.demo.ticket.webapp.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.demo.ticket")
 @ImportResource(locations = "classpath:/org/example/demo/ticket/webapp/webappContext.xml")
 public class SpringConfiguration {
+	@Autowired
+	DataSource dataSource;
 
+	@Bean("jdbcTemplate")
+	public JdbcTemplate getJdbcTemplate() {
+		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean("namedParameterJdbcTemplate")
+	public NamedParameterJdbcTemplate getNamedParamJdbcTemplate() {
+		return new NamedParameterJdbcTemplate(dataSource);
+	}
 }
