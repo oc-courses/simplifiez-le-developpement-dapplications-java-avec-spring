a5bfda7e0f18d2f6dfc9e8f92e84df7231d9b365
PierreSQS <nomo_messina@web.de>
2020-07-08 20:55:05 +0200

    Commit 28annjava-OK: Réglé exception du commit précédent.

--- ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/configuration/SpringConfiguration.java
+++ ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/configuration/SpringConfiguration.java
@@ -3,6 +3,7 @@ package org.example.demo.ticket.webapp.configuration;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,16 +15,18 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 @ComponentScan(basePackages = "org.example.demo.ticket")
 @ImportResource(locations = "classpath:/org/example/demo/ticket/webapp/webappContext.xml")
 public class SpringConfiguration {
+	
 	@Autowired
-	DataSource dataSource;
+	@Qualifier("dataSourceTicket")
+	private DataSource dataSource;
 
 	@Bean("jdbcTemplate")
-	public JdbcTemplate getJdbcTemplate() {
+	public JdbcTemplate getJdbcTemplate(@Qualifier("dataSourceTicket") DataSource dataSource) {
 		return new JdbcTemplate(dataSource);
 	}
 
 	@Bean("namedParameterJdbcTemplate")
-	public NamedParameterJdbcTemplate getNamedParamJdbcTemplate() {
+	public NamedParameterJdbcTemplate getNamedParamJdbcTemplate( @Qualifier("dataSourceTicket") DataSource dataSource ) {
 		return new NamedParameterJdbcTemplate(dataSource);
 	}
 }
