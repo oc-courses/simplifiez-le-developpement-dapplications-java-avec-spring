67be0451658522d8e6292c9086d7a96001c46f62
PierreSQS <nomo_messina@web.de>
2020-07-08 21:03:00 +0200

    Commit 29annjava-OK: Alternative au Commit28

--- ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/configuration/SpringConfiguration.java
+++ ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/configuration/SpringConfiguration.java
@@ -2,7 +2,6 @@ package org.example.demo.ticket.webapp.configuration;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,15 +9,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.example.demo.ticket")
 @ImportResource(locations = "classpath:/org/example/demo/ticket/webapp/webappContext.xml")
 public class SpringConfiguration {
 	
-	@Autowired
-	@Qualifier("dataSourceTicket")
-	private DataSource dataSource;
+	 @Bean(name="dataSourceTicket")
+     public DataSource getDataSource(){
+		 return new DriverManagerDataSource();
+         
+	 }
 
 	@Bean("jdbcTemplate")
 	public JdbcTemplate getJdbcTemplate(@Qualifier("dataSourceTicket") DataSource dataSource) {
