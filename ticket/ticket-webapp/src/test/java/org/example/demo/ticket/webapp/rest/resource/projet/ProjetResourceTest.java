d5c934feb08fe8e7ca48c65f6069a87599c0096f
PierreSQS <nomo_messina@web.de>
2020-08-02 18:06:18 +0200

    Commit 37annjava-NOK: Ajouts nécessaires pour démarrer le test.
    Test démarre mais resource projets introuvables (Erreur Http 404).

--- ticket/ticket-webapp/src/test/java/org/example/demo/ticket/webapp/rest/resource/projet/ProjetResourceTest.java
+++ ticket/ticket-webapp/src/test/java/org/example/demo/ticket/webapp/rest/resource/projet/ProjetResourceTest.java
@@ -1,33 +1,38 @@
 package org.example.demo.ticket.webapp.rest.resource.projet;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.example.demo.ticket.webapp.configuration.SpringTestConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import org.example.demo.ticket.webapp.configuration.SpringConfiguration;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 
 
-@SpringJUnitWebConfig(classes = SpringConfiguration.class)
+@SpringJUnitWebConfig(classes = SpringTestConfiguration.class)
 public class ProjetResourceTest {
 
 	MockMvc mockMvc;
 
 	@BeforeEach
-	public void setup(WebApplicationContext wac) {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+//	public void setup(WebApplicationContext wac) {
+//		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+//	}
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(new ProjetResource()).build();
 	}
 
 	@Test
-	public void getAccount() throws Exception {
-		this.mockMvc.perform(get("/accounts/1").accept(MediaType.APPLICATION_JSON))
+	@Disabled("Not Ready yet")
+	public void testgetProjets() throws Exception {
+		this.mockMvc.perform(get("/projets").accept(MediaType.APPLICATION_JSON))
 							.andExpect(status().isOk())
 							.andExpect(content().contentType("application/json"))
 							.andExpect(jsonPath("$.name").value("Lee"));
